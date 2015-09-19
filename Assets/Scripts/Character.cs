bc27cd676194dc06200019d793ea3e4c08c052b1
andSpace <dtcs400@gmail.com>
2015-09-18 20:33:34 -0500

    Initial commit

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+
+    private Vector3 pos = new Vector3(0,2,0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        pos.x = pos.x + Input.GetAxis("Horizontal");
+        pos.z = pos.z + Input.GetAxis("Vertical");
+        transform.position = pos;
+	}
+}
